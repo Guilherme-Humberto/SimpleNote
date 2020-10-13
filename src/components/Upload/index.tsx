9b95ad308334f982c7f8fddacfc7190ca3629a32
Guilherme-Humberto <gbreu@outlook.com>
2020-10-13 12:52:02 -0300

    Zona de upload de imagens

---
+++ src/components/Upload/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const Upload: React.FC = () => {
+  return (
+    <Container>
+      <h1>Upload</h1>
+    </Container>
+  );
+};
+
+export default Upload;
