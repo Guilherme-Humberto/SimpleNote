b0552d0d6e7dce9c247d43f16148d61ab4b132e1
Guilherme-Humberto <gbreu@outlook.com>
2020-11-06 16:02:44 -0300

    Implementando swr

--- src/hooks/Fetcher.ts
+++ src/hooks/Fetcher.ts
@@ -1,12 +1,18 @@
 import useSwr from 'swr'
 import api from '../services/api'
 
-export default function Fetcher<Data = any, Error = any>(url: string) {
+export default function Fetcher<Data = any, Error = any>(url: string, token: string) {
     const { data, error, mutate } = useSwr<Data, Error>(url, async url => {
-        const response = await api.get(url)
+        const response = await api.get(url, {
+            headers: {
+                "Authorization": `Bearer ${token}`
+            }
+        })
         const data = await response.data
 
         return data
+    }, {
+        refreshInterval: 500
     })
 
     return { data, error, mutate }
